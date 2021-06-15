7b6eb297769b155887a3ca0178f5a9e7c565ce27
dhairyajain571 <60997101+dhairyajain571@users.noreply.github.com>
2021-06-15 11:57:00 +0530

    contact commit

--- main.js
+++ main.js
@@ -19,3 +19,11 @@ $(".btn").click(function(){
 	$(this).toggleClass("animate");
 	$(".input").val("");
   });
+
+  function myMap() {
+	var mapProp= {
+	center:new google.maps.LatLng(51.508742,-0.120850),
+	zoom:5,
+   };
+   var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+   }
